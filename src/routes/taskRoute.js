311f3610f39c1962f7c2af9ae30e41e59d835fab
Chandni-22 <chandni22@navgurukul>
2025-04-19 01:20:47 +0530

    Project done, all the errors solved

--- src/routes/taskRoute.js
+++ src/routes/taskRoute.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 const { createTaskByUser, getTasksByUser, getAllTasksByAdmin} = require('../controllers/taskController');
 const auth = require('../middleware/authMiddleware');
-const restrictTo = require('../middleware/roleMiddleware');
+const restrict = require('../middleware/roleMiddleware');
 
 // User routes
-router.post('/', auth, restrictTo('user'), createTaskByUser);
-router.get('/user/dashboard', auth, restrictTo('user'), getTasksByUser);
+router.post('/', auth, restrict('user'), createTaskByUser);
+router.get('/user/dashboard', auth, restrict('user'), getTasksByUser);
 
 // Admin routes
-router.get('/admin/dashboard', auth, restrictTo('admin'), getAllTasksByAdmin);
+router.get('/admin/dashboard', auth, restrict('admin'), getAllTasksByAdmin);
 
 module.exports = router;
